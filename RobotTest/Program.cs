29abb2fc22015170dbc8cd4cf25f7e340f64906e
aawee0 <aawee0@gmail.com>
2016-04-09 15:11:56 +1200

    first commit

---
+++ RobotTest/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            string line = null;
+
+            Robot newRobot = new Robot();
+
+            // Intro and question about input from file
+            Console.WriteLine("Welcome to the RobotTest. This simple demo is a simulation of a toy robot moving on a square tabletop, of dimensions 5 units x 5 units. The robot is free to roam around the surface of the table, but is prevented from falling to destruction. Any movement that would result in the robot falling from the table must be prevented, however further valid movement commands are still allowed.\n");
+            string yesOrNo = null;
+            while (yesOrNo != "Y" && yesOrNo != "N") {
+                Console.Write("Do you want to input commands from a file? (Y/N) ");
+                yesOrNo = Console.ReadLine();
+                yesOrNo = yesOrNo.ToUpper();
+            }
+
+            // File name dialogue
+            System.IO.StreamReader file = null;
+            if (yesOrNo == "Y") {
+                string filename;
+                bool fileopened = false;
+
+                while (!fileopened)
+                {
+                    Console.Write("Please type in the name of the file (e.g. tests.txt): ");
+                    filename = Console.ReadLine();
+
+                    try
+                    {
+                        file = new System.IO.StreamReader(filename);
+                        if (file!=null) fileopened = true;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("The file could not be read. Please check the name or try to use an absolute path.");
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+            else if (yesOrNo == "N") Console.WriteLine("\nNow entering the console mode. Please start from placing the robot on the table by using PLACE X,Y,F command, or type HELP for more details.\n");
+
+            while (!exit)
+            {
+                if (yesOrNo == "Y")
+                {
+                    line = file.ReadLine();
+                    Console.WriteLine(line);
+                    if (line == null)
+                    {
+                        Console.WriteLine("Finished processing. Please type any required command or finish by using EXIT.");
+                        yesOrNo = "N";
+                    }
+                }
+                else if (yesOrNo == "N") line = Console.ReadLine();
+
+                if (line != null)
+                {
+                    line = line.ToUpper();
+                    switch (line)
+                    {
+                        case "EXIT":
+                            Console.WriteLine("Exiting the program..");
+                            exit = true;
+                            break;
+                        case "HELP":
+                            Console.WriteLine("\nAvaliable commands:");
+                            Console.WriteLine("PLACE X,Y,F - to place the robot, where: \n - X and Y are numbers from 0 to 4 \n - F is a direction (NORTH, EAST, SOUTH or WEST);");
+                            Console.WriteLine("MOVE to move the robot one unit forward in the direction currently facing;");
+                            Console.WriteLine("LEFT and RIGHT to rotate the robot in the specified direction;");
+                            Console.WriteLine("REPORT to display the coordinates and the direction of the robot.\n");
+                            // WRITE HELP MENU
+                            break;
+                        case "":
+                            break;
+                        default:
+                            string output = newRobot.cmdExec(line);
+                            if (output != "") Console.WriteLine(output);
+                            break;
+                    }
+                }
+
+
+            }
+
+
+        }
+    }
+}
---
+++ RobotTest/Robot.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RobotTest
+{
+    class Robot
+    {
+        public int xCrd { get; set; }
+        public int yCrd { get; set; }
+        public string dirFace { get; set; }
+        public bool placed { get; set; }
+
+        public Robot() {
+            placed = false;
+        }
+
+        public string cmdExec(string inpCommand) {
+            string command = System.Text.RegularExpressions.Regex.Replace(inpCommand.ToUpper(), " ", "");
+
+            // check if it's a place command
+            Regex regex = new Regex(@"PLACE(\d),(\d),(\S*)");
+            Match match = regex.Match(command);
+            if (match.Success)
+            {
+                int xNew = Convert.ToInt16(match.Groups[1].Value);
+                int yNew = Convert.ToInt16(match.Groups[2].Value);
+                string fNew = match.Groups[3].Value;
+
+                // execute place
+                int placeRes = cmdPlace(xNew, yNew, fNew);
+
+                if (placeRes == 0) return "\nInvalid coordinate: please choose X and Y from 0 to 4.\n";
+                else if (placeRes == -1) return "\nInvalid direction: please choose from NORTH, EAST, SOUTH or WEST.\n";
+                else return "";
+            }
+            else
+            {
+                // execute another command
+                if (placed) {
+                    switch (command)
+                    {
+                        case "MOVE":
+                            cmdMove();
+                            break;
+                        case "LEFT":
+                            cmdLeft();
+                            break;
+                        case "RIGHT":
+                            cmdRight();
+                            break;
+                        case "REPORT":
+                            return cmdOutput();
+                        default:
+                            return "\nInvalid command: use HELP for the list of available commands.\n";
+                    }
+                    return "";
+                }
+                else return "\nRobot should be placed first. Please use the PLACE X,Y,F command, where: \n X and Y are numbers from 0 to 4 \n F is a direction (NORTH, EAST, SOUTH or WEST)\n"; 
+            }
+
+        }
+
+        public int cmdPlace(int X, int Y, string F) {
+            // returns:
+            // 1 if placed successfull
+            // 0 if coordinate is invalid
+            // -1 if direction is invalid
+
+            if (X <= 4 && X >= 0 && Y <= 4 && Y >= 0)
+            {
+                xCrd = X;
+                yCrd = Y;
+            }
+            else return 0;
+
+            if (F == "NORTH" || F == "EAST" || F == "SOUTH" || F == "WEST") dirFace = F;
+            else return -1;
+
+            placed = true;
+            return 1;
+        }
+
+        public int cmdMove() {
+            // returns:
+            // 1 if moved successfull
+            // 0 if hit the border
+
+            int xNew = xCrd;
+            int yNew = yCrd;
+
+            switch (dirFace)
+            {
+                case "NORTH":
+                    yNew ++;
+                    break;
+                case "SOUTH":
+                    yNew --;
+                    break;
+                case "WEST":
+                    xNew --;
+                    break;
+                case "EAST":
+                    xNew ++;
+                    break;
+            }
+            if (xNew <= 4 && xNew >= 0 && yNew <= 4 && yNew >= 0)
+            {
+                xCrd = xNew;
+                yCrd = yNew;
+                return 1;
+            }
+            else return 0;
+            
+        }
+
+        public void cmdLeft()
+        {
+            switch (dirFace)
+            {
+                case "NORTH":
+                    dirFace = "WEST";
+                    break;
+                case "WEST":
+                    dirFace = "SOUTH";
+                    break;
+                case "SOUTH":
+                    dirFace = "EAST";
+                    break;
+                case "EAST":
+                    dirFace = "NORTH";
+                    break;
+            }
+        }
+
+        public void cmdRight()
+        {
+            switch (dirFace)
+            {
+                case "NORTH":
+                    dirFace = "EAST";
+                    break;
+                case "EAST":
+                    dirFace = "SOUTH";
+                    break;
+                case "SOUTH":
+                    dirFace = "WEST";
+                    break;
+                case "WEST":
+                    dirFace = "NORTH";
+                    break;
+            }
+        }
+
+        public string cmdOutput() {
+            return "\nOutput: " + xCrd.ToString() + "," + yCrd.ToString() + "," + dirFace + "\n";
+        }
+
+    }
+}
